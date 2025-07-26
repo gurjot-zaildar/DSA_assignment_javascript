8da842eba8c10ae7849eaee5c4c49e37b7c31c12
gurjot-zaildar <signingames115@gmail.com>
2025-07-26 17:05:53 +0530

    solved question 1 or 2

---
+++ assignment_3/question_1/script.js
@@ -0,0 +1,7 @@
+/*  Given a string, print each character on a separate line.*/
+
+let str = prompt("enter your name");
+
+for(let i=0;i<str.length;i++){
+    console.log(str[i])
+}
---
+++ assignment_3/question_2/script.js
@@ -0,0 +1,7 @@
+let str = prompt("enter your name");
+let strev="";
+    for(let j=str.length-1; j>=0;j--){
+     strev+=str[j]
+    }
+
+console.log(strev)
